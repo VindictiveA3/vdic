48fac8284241e82255b710c11fc056e72b6d04b9
Ryan <ryanwolfe556@gmail.com>
2018-03-26 17:43:56 -0500

    Removed Broken Texture from Med Orca

--- vdicklife.Altis/config/Config_Vehicles.hpp
+++ vdicklife.Altis/config/Config_Vehicles.hpp
@@ -826,9 +826,6 @@ will modify the virtual space and the price of the vehicle, but other informatio
             { "Desert Digi", "reb", {
                 "\a3\air_f\Heli_Light_02\Data\heli_light_02_ext_opfor_co.paa"
             }, "" },
-			{ "EMS Blue", "med", {
-                "textures\medic_textures\medic_vehicles\emsbird.paa"
-            }, "" },
             { "EMS White", "med", {
                 "#(argb,8,8,3)color(1,1,1,0.8)"
             }, "" }
